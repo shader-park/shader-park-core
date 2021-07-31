68b3ef72d42d76a58073de034778e5071f579c5f
Torin Blankensmith <torin.blankensmith@gmail.com>
2021-07-31 16:46:51 -0400

    update version

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ import {
     defaultFragSourceGLSL
 } from './glsl/glsl-lib.js'
 
-console.log('using shader-park version: 0.0.271');
+console.log('using shader-park version: 0.0.28');
 
 /// Generate code for various targets
 
